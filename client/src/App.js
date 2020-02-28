9cbf752ac2ab4a76ac5dde7976743cff724b46f2
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-28 11:05:36 +0200

    complete-app-correct-private-route-file

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,41 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./components/PrivateRoute";
 import "./styles.scss";
+import styled from "styled-components";
+
+const AppStyles = styled.div`
+  margin: 0 auto;
+  background: linear-gradient(
+    230deg,
+    #fff,
+    #ff652f,
+    #ff652f,
+    #ffe400,
+    #ffe400,
+    #14a76c,
+    #14a76c
+  );
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <Router>
+    <AppStyles>
+      <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute exact path="/bubbles" component={BubblePage}/>
       </div>
     </Router>
+    </AppStyles>
+    
   );
 }
 
